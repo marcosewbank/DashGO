374ba55ef311ae05a2845be29cd643b0036562b0
marcos ewbank <marcos.suarez@pivotree.com>
2021-09-28 07:05:13 -0300

    sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function dashboard() {
   return (
-    <div>
+    <Flex direction="column" h="100vh">
       <Header />
-    </div>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
