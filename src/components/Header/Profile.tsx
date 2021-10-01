612d2ddc22b2462445fbe4ee11b5bbdf98f17be1
marcos ewbank <marcos.suarez@pivotree.com>
2021-10-01 07:10:29 -0300

    header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Flex, Text, Box, Avatar } from "@chakra-ui/react";
 
-export const Profile = () => {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+export const Profile = ({ showProfileData }: ProfileProps) => {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Marcos Ewbank</Text>
-        <Text color="gray.300" fontSize="small">
-          ewbankmarcos@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Marcos Ewbank</Text>
+          <Text color="gray.300" fontSize="small">
+            ewbankmarcos@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
